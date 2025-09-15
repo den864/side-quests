d2cd46186a8c31245f5cb0fe818140e7f61916e3
den864 <nsnszjz@gmail.com>
2025-09-15 10:13:11 +0530

    proffession management codes added

---
+++ profession-management-system/script.js
@@ -0,0 +1,88 @@
+const userArr=[];
+let id=1;
+
+
+const form=document.querySelector("form");
+
+form.addEventListener("submit",(event)=>{
+    event.preventDefault();
+
+    const data=new FormData(form);
+
+    const name=data.get("username").trim();
+    const profession=data.get("userprofession").trim();
+    const age=data.get("userage").trim();
+
+    if(!name || !profession || !age){
+        document.querySelector(".success").style.display="none";
+        document.querySelector(".error").style.display="block";
+        return;
+    }else{
+        document.querySelector(".error").style.display="none";
+    }
+    
+    const obj={
+        id:id,
+        name:name,
+        profession:profession,
+        age:age
+    }
+    id++;
+    userArr.push(obj);
+    document.querySelector(".success").style.display="block";
+    
+    renderData();
+    form.reset();
+})
+
+renderData();
+
+
+function renderData(){
+    const target=document.querySelector(".employee-data");
+    target.innerHTML="";
+
+    if(userArr.length===0){
+        const div=document.createElement("div");
+        div.textContent="No Employee added";
+        target.appendChild(div);
+        return;
+    }
+
+    userArr.forEach(user=>{
+        const div=document.createElement("div");
+        div.classList.add("employee-details");
+
+        let html="";
+
+        for(let key in user){
+        
+            if(key==='id'){
+                html+=`<div>${user[key]}</div>`;
+            }else{
+                html+=`<div>${key}: ${user[key]}</div>`;
+            }
+        }
+        div.innerHTML=html;
+
+
+        const deleteBtn=document.createElement("button");
+        deleteBtn.textContent="Delete";
+        deleteBtn.addEventListener("click",()=>{
+            deleteUser(user.id);
+        })
+
+        div.appendChild(deleteBtn);
+
+        target.appendChild(div);
+    })
+}
+
+function deleteUser(userid){
+    const index=userArr.findIndex(user=>user.id===userid);
+
+    if(index!=-1){
+        userArr.splice(index,1);
+    }
+    renderData();
+}
